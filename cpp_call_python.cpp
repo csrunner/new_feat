7f3b95f94456341a169c939fa19e4faf4dc2a4ee
csrunner <chaoshi_cn@hotmail.com>
2019-02-21 20:18:19 +0800

    fix c++ call python bug

--- cpp_call_python.cpp
+++ cpp_call_python.cpp
@@ -4,26 +4,31 @@ int great_function_from_python(int a) {
     int res;
     PyObject *pModule,*pFunc;
     PyObject *pArgs, *pValue;
-    
+    PyRun_SimpleString("import sys");
+    PyRun_SimpleString("sys.path.append('./')");
     /* import */
     pModule = PyImport_Import(PyString_FromString("cpp_python_module"));
 
     /* great_module.great_function */
-    pFunc = PyObject_GetAttrString(pModule, "cpp_python_func"); 
+    pFunc = PyObject_GetAttrString(pModule, "cpp_call_python_func"); 
     
     /* build args */
     pArgs = PyTuple_New(1);
     PyTuple_SetItem(pArgs,0, PyInt_FromLong(a));
       
     /* call */
+    
     pValue = PyObject_CallObject(pFunc, pArgs);
     
     res = PyInt_AsLong(pValue);
     return res;
+    
 }
 
 int main(int argc, char *argv[]) {
     Py_Initialize();
-    printf("%d",great_function_from_python(2));
+    printf("%d\n",great_function_from_python(2));
+    // great_function_from_python(2);
     Py_Finalize();
+    return 0;
 }
