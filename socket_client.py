ec9894f4fd1acf375e7ea991f2b5c244894588fe
csrunner <chaoshi_cn@hotmail.com>
2019-03-01 19:59:46 +0800

    simple socket demo

--- socket_client.py
+++ socket_client.py
@@ -2,24 +2,32 @@
 __author__ = 'shichao'
 
 import socket
-import cv2
-img = cv2.imread('/Users/shichao/Downloads/1.jpg')
 
+'''
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # 建立连接:
 s.connect(('127.0.0.1', 9999))
 # 接收欢迎消息:
 print(s.recv(1024).decode('utf-8'))
-# for data in [b'Michael', b'Tracy', b'Sarah']:
+for data in [b'Michael', b'Tracy', b'Sarah']:
     # 发送数据:
-    # s.send(data)
-    # print(s.recv(1024).decode('utf-8'))
+    s.send(data)
+    print(s.recv(1024).decode('utf-8'))
+s.send(b'exit')
+s.close()
 
-s.send(img)
-blue = s.recv(1024)
-# print(blue.shape)
-# cv2.imshow('client win',blue)
-# cv2.waitKey(10000)
-# cv2.destroyAllWindows()
+'''
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# 建立连接:
+s.connect(('127.0.0.1', 9999))
+# 接收欢迎消息:
+print(s.recv(1024).decode('utf-8')+'received')
+# for data in [b'Michael', b'Tracy', b'Sarah']:
+#     # 发送数据:
+#     s.send(data)
+#     print(s.recv(1024).decode('utf-8'))
+data = r'1'
+s.send(data)
+print(s.recv(1024).decode('utf-8'))
 s.send(b'exit')
 s.close()
--- socket_server.py
+++ socket_server.py
@@ -4,27 +4,50 @@ __author__ = 'shichao'
 import socket
 import threading
 import time
-import cv2
+import numpy as np
 
 
+'''
 def tcplink(sock, addr):
     print('Accept new connection from %s:%s...' % addr)
     sock.send(b'Welcome!')
     while True:
         data = sock.recv(1024)
-        print(data)
-        # cv2.imshow('recv img',data)
-        # cv2.waitKey(10000)
-        # cv2.destroyAllWindows()
-        # time.sleep(1)
-        # if not data or data.decode('utf-8') == 'exit':
-        #     break
-        sock.send(data)
-        # print(('server Hello, %s!' % data.decode('utf-8')).encode('utf-8'))
-        # sock.send(('Hello, %s!' % data.decode('utf-8')).encode('utf-8'))
+        time.sleep(1)
+        if not data or data.decode('utf-8') == 'exit':
+            break
+        sock.send(('Hello, %s!' % data.decode('utf-8')).encode('utf-8'))
     sock.close()
     print('Connection from %s:%s closed.' % addr)
-    exit()
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# 监听端口
+s.bind(('127.0.0.1', 9999))
+s.listen(5)
+print('Waiting for connection...')
+
+while True:
+    # 接手一个新连接:
+    sock, addr = s.accept()
+    t = threading.Thread(target=tcplink, args=(sock, addr))
+    t.start()
+'''
+def tcplink(sock, addr):
+    print('Accept new connection from %s:%s...' % addr)
+    sock.send(b'Welcome!')
+    while True:
+        data = sock.recv(1024)
+        img = np.fromstring(data,dtype='uint8')
+        print('img: {0}'.format(img))
+        time.sleep(1)
+        if not data or data.decode('utf-8') == 'exit':
+            break
+        sock.send(('Hello, %s!' % data.decode('utf-8')).encode('utf-8'))
+    sock.close()
+    print('Connection from %s:%s closed.' % addr)
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
